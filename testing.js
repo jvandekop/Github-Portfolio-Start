e7dbd81e0cc8184ec5d6275f5b4ee7602d07ce1d
Jorden Vande Kop <gamingledger@gmail.com>
2024-01-17 13:36:17 -0600

    changed console log

--- testing.js
+++ testing.js
@@ -6,5 +6,5 @@ const secondNum = firstNum + 2;
 
 const favAnimal = prompt("What is your favorite animal");
 
-console.log(secondNum);
-console.log(favAnimal);
+console.log("secondNum", secondNum);
+console.log("favAnimal", favAnimal);
