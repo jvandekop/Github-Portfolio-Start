4cec167f4b5d15df7cb64b7aa1749e6e875ad345
Jorden Vande Kop <gamingledger@gmail.com>
2024-01-28 23:19:10 -0600

    arrays html and js

---
+++ arrays.js
@@ -0,0 +1,26 @@
+const myArray = [10, 20, 30, 40];
+
+const addOneAndLog = function(num) {
+    const result = num + 1;
+    console.log(result);
+}
+
+function checkEvenOrOdd(callback, num) {
+    if (num % 2 === 0) {
+        callback(num);
+        } else {
+            console.log("The number is odd");
+        }
+}
+
+const logNumber = function(num) {
+    console.log("the number is", num);
+}
+
+for (let index in myArray) {
+    checkEvenOrOdd(logNumber, myArray[index]);
+}
+
+addOneAndLog(myArray[0]);
+checkEvenOrOdd(logNumber, 5);
+checkEvenOrOdd(logNumber, 6);
