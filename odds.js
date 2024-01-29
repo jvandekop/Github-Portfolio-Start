6db0e6f910d6fc9e0fa7b8891b760a8675584f4d
Jorden Vande Kop <gamingledger@gmail.com>
2024-01-28 22:49:42 -0600

    added odd number

---
+++ odds.js
@@ -0,0 +1,13 @@
+//Name: Jorden Vande Kop
+//Date: 1/28/2024
+
+
+function loopNum(limit) {
+    for(let i = 1; i <limit; i++) {
+        if (i % 2 !== 0) {
+        console.log(i)
+        }
+    }
+}
+
+loopNum(6);
